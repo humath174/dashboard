1b2f7ce54e0ad0005f1919fd588f7e55ddf340d6
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-01-06 22:30:55 +0100

    Ajout d'une page de connexion sur la gestion des utilisateurs

--- back/dashboard/back/index.php
+++ back/dashboard/back/index.php
@@ -1,3 +1,18 @@
+<?php
+// Démarrez la session
+session_start();
+
+// Vérifiez si la variable de session 'username' existe
+if (isset($_SESSION['username'])) {
+    // L'utilisateur a une session ouverte
+    echo "Bienvenue, " . $_SESSION['username'] . "!";
+} else {
+    // Redirigez l'utilisateur vers la page de connexion
+    header("Location: connexion.php");
+    exit(); // Assurez-vous d'arrêter l'exécution du script après la redirection
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- back/dashboard/index.php
+++ back/dashboard/index.php
@@ -1,9 +1,12 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Site de Liens</title>
+
+    <title>Dashboard</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -30,7 +33,7 @@
 </head>
 <body>
 
-<h1>Liens Utiles</h1>
+<h1>Dashboard </h1>
 
 <ul>
     <li><a href="bbra/welcome.php" target="_blank">Dashboard Bbra</a></li>
