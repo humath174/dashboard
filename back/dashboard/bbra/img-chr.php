e9dbecdf2d10586e0b8dac704950006cba99868b
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-01-14 19:44:37 +0100

    Avancement navabr

--- back/dashboard/bbra/img-chr.php
+++ back/dashboard/bbra/img-chr.php
@@ -25,6 +25,8 @@ if (isset($_SESSION['username'])) {
     <meta name="description" content="Bruno Broyer Reflets Amenagements" />
     <meta name="keywords" content="pisciniste, bruno broyer reflets amenagements, bruno broyer, amenagements, piscine chatillon sur chalaronne, chatillon sur chalaronne, ain, rhone, auvergne rhone-alpes" />
     <meta name="author" content="DigitalGroup" />
+    <link rel="stylesheet" href="/css/style-nav.css">
+    <script type="text/javascript" src="/js/annim.js"></script>
     <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS externe -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -49,33 +51,9 @@ if (isset($_SESSION['username'])) {
 </script>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Administration</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active text-dark" aria-current="page" href="welcome.php">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="devis-rec.php">Devis</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark"  href="contact-rec.php">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark"  href="img-chr.php">Charger une Image</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="/site/index.html">Retourner sur le site</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php
+include('navbar.php');
+?>
 
 <br><br>
 <form action="upload.php" method="post" enctype="multipart/form-data">
