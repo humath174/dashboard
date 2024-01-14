eb54ac05dc7f40f304dc918d2be9aa8a740b829f
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-01-14 18:47:07 +0100

    Update welcome.php

--- back/dashboard/bbra/welcome.php
+++ back/dashboard/bbra/welcome.php
@@ -24,8 +24,8 @@ if (isset($_SESSION['username'])) {
     <meta name="description" content="Bruno Broyer Reflets Amenagements" />
     <meta name="keywords" content="pisciniste, bruno broyer reflets amenagements, bruno broyer, amenagements, piscine chatillon sur chalaronne, chatillon sur chalaronne, ain, rhone, auvergne rhone-alpes" />
     <meta name="author" content="DigitalGroup" />
-    <link rel="stylesheet" href="/css/style-nav.css">
-    <script type="text/javascript" src="/js/annim.js"></script>
+    <link rel="stylesheet" href="/bbra/css/style-nav.css">
+    <script type="text/javascript" src="/bbra/js/annim.js"></script>
     <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS externe -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
