83ddc0f17fa3047549d8338f0dcc93e5f36c0fdd
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-01-28 21:38:07 +0100

    Update addstudent.php

--- back/dashboard/bbra/component/addstudent.php
+++ back/dashboard/bbra/component/addstudent.php
@@ -2,13 +2,6 @@
     include './bbra/pages/conixion.php';
     if(isset($_POST['submit'])){
         
-        $image = $_FILES['img']['name'];
-        $tempname = $_FILES['img']['tmp_name'];  
-        $folder = "../assets/img/".$image;
-        
-        if(move_uploaded_file($tempname,$folder)){
-            echo 'images est uplade';
-        }
 
         $Name = $_POST['Name'];
         $Email = $_POST['Email'];
