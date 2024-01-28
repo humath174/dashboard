78bd64668f63713bf63808648ac0879883e2e719
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-01-28 19:53:43 +0100

    Update welcome.php

--- back/dashboard/bbra/welcome.php
+++ back/dashboard/bbra/welcome.php
@@ -1,16 +1,3 @@
-<?php
-// Démarrez la session
-session_start();
-
-// Vérifiez si la variable de session 'username' existe
-if (isset($_SESSION['username'])) {
-    // L'utilisateur a une session ouverte
-     else {
-    // Redirigez l'utilisateur vers la page de connexion
-    header("Location: connexion.php");
-    exit(); // Assurez-vous d'arrêter l'exécution du script après la redirection
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
