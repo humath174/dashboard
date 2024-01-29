f86c20bf3f4f3446151f4d39801cd8ade8a77719
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-01-29 19:49:27 +0100

    Update index.php

--- back/dashboard/bbra/index.php
+++ back/dashboard/bbra/index.php
@@ -1,4 +1,12 @@
-<?php
-header(Location : . $dashboard.matheo.site/welcome.php);
-Exit();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0;url=welcome.php">
+    <title>Redirection en cours...</title>
+</head>
+<body>
+<!-- Optionnel : Ajoutez un message de redirection si le navigateur ne prend pas en charge la redirection automatique -->
+<p>Si vous n'êtes pas redirigé, <a href="welcome.php">cliquez ici</a>.</p>
+</body>
+</html>
