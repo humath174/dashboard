3222dbcca88adc2633113da5f00d3378956b8859
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-02-11 16:05:42 +0100

    k

---
+++ back/dashboard/bbra/public/database/database.php
@@ -0,0 +1,7 @@
+<?php
+$serveur = "localhost"; // Adresse du serveur MySQL
+$utilisateur = "monty"; // Nom d'utilisateur MySQL
+$motDePasse = "some_pass"; // Mot de passe MySQL
+$baseDeDonnees = "gestion"; // Nom de la base de données
+$baseDeDonnees = "dashboard"; // Nom de la base de données
+?>
