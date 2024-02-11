0fbfff5d8e51c3335ebafb4a06185f9670cc0c63
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-02-11 16:54:38 +0100

    Update database.php

--- back/dashboard/bbra/public/database/database.php
+++ back/dashboard/bbra/public/database/database.php
@@ -2,6 +2,5 @@
 $serveur = "localhost"; // Adresse du serveur MySQL
 $utilisateur = "monty"; // Nom d'utilisateur MySQL
 $motDePasse = "some_pass"; // Mot de passe MySQL
-$baseDeDonnees = "gestion"; // Nom de la base de données
-$baseDeDonnees = "dashboard"; // Nom de la base de données
+$baseDeDonnees = "bbra"; // Nom de la base de données
 ?>
