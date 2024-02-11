aac634fc7374fa6dc0fcc754b02ce42544c41807
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-02-11 12:37:55 +0100

    Modifcatio head

--- back/dashboard/bbra/fichier_client.php
+++ back/dashboard/bbra/fichier_client.php
@@ -11,14 +11,9 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
           integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0HXKBBMW06"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-0HXKBBMW06');
-    </script>
+    <?php
+include ('component/head.php')
+ ?>
 </head>
 
 <body class="bg-content">
--- back/dashboard/bbra/index.php
+++ back/dashboard/bbra/index.php
@@ -5,14 +5,9 @@
     <meta http-equiv="refresh" content="0;url=welcome.php">
     <title>Redirection en cours...</title>
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0HXKBBMW06"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-0HXKBBMW06');
-    </script>
+    <?php
+include ('component/head.php')
+ ?>
 </head>
 <body>
 <!-- Optionnel : Ajoutez un message de redirection si le navigateur ne prend pas en charge la redirection automatique -->
