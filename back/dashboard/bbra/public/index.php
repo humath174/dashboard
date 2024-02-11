399c53b2a187707528d54ca22c8104ef1b6b238e
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-02-11 16:25:05 +0100

    Update index.php

--- back/dashboard/bbra/public/index.php
+++ back/dashboard/bbra/public/index.php
@@ -274,17 +274,19 @@ include ('assets/sidebar.php')
                 <?php include 'component/popupadd.php'; ?>
             </div>
         </div>
-        <div class="table-responsive">
-            <table class="table student_list table-borderless">
+        <div class="w-full overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+            <table class="w-full whitespace-no-wrap">
                 <thead>
-                <tr class="align-middle">
-                    <th class="opacity-0">vide</th>
-                    <th>Nom</th>
-                    <th>Prenom</th>
-                    <th>Mail</th>
-                    <th>Telephone</th>
-                    <th>Description</th>
-                    <th class="opacity-0">list</th>
+                <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+                    >
+                    
+                    <th class="px-4 py-3">Nom</th>
+                    <th class="px-4 py-3">Prenom</th>
+                    <th class="px-4 py-3">Mail</th>
+                    <th class="px-4 py-3">Telephone</th>
+                    <th class="px-4 py-3">Description</th>
+                   
                 </tr>
                 </thead>
                 <tbody>
@@ -309,7 +311,8 @@ include ('assets/sidebar.php')
                 </tbody>
             </table>
         </div>
-
+        </div>
+          
 
             <!-- Charts -->
             <h2
