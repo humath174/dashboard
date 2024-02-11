9da02cb98fce91fe5f349966e68aed26ad536eaf
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-02-11 16:07:40 +0100

    Update index.php

--- back/dashboard/bbra/public/index.php
+++ back/dashboard/bbra/public/index.php
@@ -185,55 +185,7 @@ include ('assets/sidebar.php')
                     <tr
                       class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
                     >
-                      <th class="px-4 py-3">Client</th>
-                      <th class="px-4 py-3">Email</th>
-                      <th class="px-4 py-3">Telephone</th>
-                      <th class="px-4 py-3">Description</th>
-                    </tr>
-                  </thead>
-                  <tbody
-                    class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
-                  >
-                    <tr class="text-gray-700 dark:text-gray-400">
-                      <td class="px-4 py-3">
-                        <div class="flex items-center text-sm">
-                          <!-- Avatar with inset shadow -->
-                          <div
-                            class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
-                          >
-                            <img
-                              class="object-cover w-full h-full rounded-full"
-                              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                              alt=""
-                              loading="lazy"
-                            />
-                            <div
-                              class="absolute inset-0 rounded-full shadow-inner"
-                              aria-hidden="true"
-                            ></div>
-                          </div>
-                          <div>
-                            <p class="font-semibold">Hans Burger</p>
-                            <p class="text-xs text-gray-600 dark:text-gray-400">
-                              10x Developer
-                            </p>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="px-4 py-3 text-sm">
-                        $ 863.45
-                      </td>
-                      <td class="px-4 py-3 text-xs">
-                        <span
-                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
-                        >
-                          Approved
-                        </span>
-                      </td>
-                      <td class="px-4 py-3 text-sm">
-                        6/10/2020
-                      </td>
-                    </tr>
+                     
 
                     <?php
 // Paramètres de connexion à la base de données
@@ -255,7 +207,7 @@ $resultat = $connexion->query($requete);
 if ($resultat->num_rows > 0) {
     echo "<h2>Informations des Utilisateurs</h2>";
     echo "<table>";
-    echo "<tr><th>ID</th><th>Nom</th><th>Prenom</th><th>Mail</th><th>Telephone</th><th>Description</th></tr>";
+    echo "<tr><th class="px-4 py-3">ID</th><th class="px-4 py-3">Nom</th><th class="px-4 py-3">Prenom</th><th class="px-4 py-3">Mail</th><th class="px-4 py-3">Telephone</th><th class="px-4 py-3">Description</th></tr>";
 
     // Afficher les informations des utilisateurs dans le tableau
     while ($row = $resultat->fetch_assoc()) {
